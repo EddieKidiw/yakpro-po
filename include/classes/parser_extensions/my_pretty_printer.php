3a0b44aebb933e81c70e26ff392a1d490f92cefa
EddieKidiw <eddiekidiw@gmail.com>
2022-07-14 03:47:25 +0700

    build classes

--- include/classes/parser_extensions/my_pretty_printer.php
+++ include/classes/parser_extensions/my_pretty_printer.php
@@ -1,79 +1,91 @@
 <?php
+
 namespace Eddiekidiw\YakproPo\classes\parser_extensions;
+
 //========================================================================
 // Author:  Pascal KISSIAN
 // Resume:  http://pascal.kissian.net
 //
-// Copyright (c) 2015-2020 Pascal KISSIAN
+// Copyright (c) 2015-2021 Pascal KISSIAN
 //
 // Published under the MIT License
 //          Consider it as a proof of concept!
 //          No warranty of any kind.
 //          Use and abuse at your own risks.
 //========================================================================
-
 use PhpParser;
 use PhpParser\Node;
-use PhpParser\Node\Scalar;
+use PhpParser\Node\Scalar\String_;
+use PhpParser\Node\Scalar\Encapsed;
+use PhpParser\Node\Scalar\EncapsedStringPart;
 use PhpParser\PrettyPrinter\Standard;
-
+/**
+* @class my_pretty_printer
+* @new my_pretty_printer();
+*/
 class my_pretty_printer extends Standard
 {
+    /**
+    * @function obfuscate_string
+    * @param $str
+    * @return ?
+    */
     private function obfuscate_string($str)
     {
         $l = strlen($str);
         $result = '';
-        for($i=0;$i<$l;++$i)
-        {
-            $result .= mt_rand(0,1) ? "\x".dechex(ord($str[$i])) : "\\".decoct(ord($str[$i]));
+        for ($i = 0; $i < $l; ++$i) {
+            $result .= mt_rand(0, 1) ? "\\x" . dechex(ord($str[$i])) : "\\" . decoct(ord($str[$i]));
         }
         return $result;
     }
-
-
-    public function pScalar_String(PhpParser\Node\Scalar\String_ $node)
+    /**
+    * @function pScalar_String
+    * @param String_ $node
+    * @return ?
+    */
+    public function pScalar_String(String_ $node)
     {
-        $result = $this->obfuscate_string($node->value);            if (!strlen($result)) return "''";
-        return  '"'.$this->obfuscate_string($node->value).'"';
+        $result = $this->obfuscate_string($node->value);
+        if (!strlen($result)) {
+            return "''";
+        }
+        return '"' . $this->obfuscate_string($node->value) . '"';
     }
-
-
-    //TODO: pseudo-obfuscate HEREDOC string
-    protected function pScalar_Encapsed(PhpParser\Node\Scalar\Encapsed $node)
+    /**
+    * @function pScalar_Encapsed
+    * @param Encapsed $node
+    * @return ?
+    */
+    protected function pScalar_Encapsed(Encapsed $node)
     {
         /*
-        if ($node->getAttribute('kind') === PhpParser\Node\Scalar\String_::KIND_HEREDOC) 
+        if ($node->getAttribute('kind') === String_::KIND_HEREDOC) 
         {
             $label = $node->getAttribute('docLabel');
             if ($label && !$this->encapsedContainsEndLabel($node->parts, $label)) 
             {
                 if (count($node->parts) === 1
-                    && $node->parts[0] instanceof PhpParser\Node\Scalar\EncapsedStringPart
+                    && $node->parts[0] instanceof EncapsedStringPart
                     && $node->parts[0]->value === ''
                 )
                 {
                     return "<<<$label\n$label" . $this->docStringEndToken;
                 }
-
+        
                 return "<<<$label\n" . $this->pEncapsList($node->parts, null) . "\n$label"
                      . $this->docStringEndToken;
             }
         }
         */
         $result = '';
-        foreach ($node->parts as $element)
-        {
-            if ($element instanceof PhpParser\Node\Scalar\EncapsedStringPart)
-            {
-                $result .=  $this->obfuscate_string($element->value);
-            }
-            else
-            {
+        foreach ($node->parts as $element) {
+            if ($element instanceof EncapsedStringPart) {
+                $result .= $this->obfuscate_string($element->value);
+            } else {
                 $result .= '{' . $this->p($element) . '}';
             }
         }
-        return '"'.$result.'"';
+        return '"' . $result . '"';
     }
 }
-
-?>
