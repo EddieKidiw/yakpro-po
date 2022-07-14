0b176dd16c1d018bf6ad248365f86f80d20a680b
EddieKidiw <eddiekidiw@gmail.com>
2022-07-14 16:14:04 +0700

    Update get_default_defined_objects.php

--- include/get_default_defined_objects.php
+++ include/get_default_defined_objects.php
@@ -2,6 +2,18 @@
 
 namespace Eddiekidiw\YakproPo;
 
+//========================================================================
+// Author:  Pascal KISSIAN
+// Resume:  http://pascal.kissian.net
+//
+// Copyright (c) 2015 Pascal KISSIAN
+//
+// Published under the MIT License
+//          Consider it as a proof of concept!
+//          No warranty of any kind.
+//          Use and abuse at your own risks.
+//========================================================================
+
 /**
 * @class get_default_defined_objects
 * @new get_default_defined_objects();
