698f51557a7a4770c77152adc01295cf4e9b1d7b
EddieKidiw <eddiekidiw@gmail.com>
2022-07-14 16:14:40 +0700

    Update Config.php

--- include/classes/Config.php
+++ include/classes/Config.php
@@ -2,6 +2,20 @@
 
 namespace Eddiekidiw\YakproPo\classes;
 
+//========================================================================
+// Author:  Pascal KISSIAN
+// Resume:  http://pascal.kissian.net
+//
+// Copyright (c) 2015-2022 Pascal KISSIAN
+//
+// Published under the MIT License
+//          Consider it as a proof of concept!
+//          No warranty of any kind.
+//          Use and abuse at your own risks.
+//========================================================================
+
+// when we use the word ignore, that means that it is ignored during the obfuscation process (i.e. not obfuscated)
+
 /**
 * @class Config
 * @new Config();
